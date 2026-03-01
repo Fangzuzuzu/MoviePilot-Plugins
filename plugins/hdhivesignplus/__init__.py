1422ecfc2ee4655ed40f92248055aacb57e340f0
Fangzuzuzu <30969849+Fangzuzuzu@users.noreply.github.com>
2026-03-01 21:47:09 +0800

    fix: 去除目录名和类名中的下划线，与 MoviePilot 官方命名惯例对齐
    - 目录名: hdhivesign_plus → hdhivesignplus
    - 类名: hdhivesign_plus → HdhivesignPlus (PascalCase)
    - package.json key: hdhivesign_plus → HdhivesignPlus
    - 参考原始仓库的 HdhiveSign 命名规范

--- plugins/hdhivesignplus/__init__.py
+++ plugins/hdhivesignplus/__init__.py
@@ -40,7 +40,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-class hdhivesign_plus(_PluginBase):
+class HdhivesignPlus(_PluginBase):
     # 插件名称
     plugin_name = "影巢签到修复"
     # 插件描述
@@ -54,7 +54,7 @@ class hdhivesign_plus(_PluginBase):
     # 作者主页
     author_url = "https://github.com/Fangzuzuzu"
     # 插件配置项ID前缀
-    plugin_config_prefix = "hdhivesign_plus_"
+    plugin_config_prefix = "hdhivesignplus_"
     # 加载顺序
     plugin_order = 1
     # 可使用的用户级别
